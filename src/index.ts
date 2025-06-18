0d545219eea9f559444ac2bec5f284a1cf443503
GustaVasconcelos <gustavo.vasconcelos.peixoto18@gmail.com>
2025-06-18 10:38:07 -0300

    feat: adiciona pasta public

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { connectMongo, mongoConnection } from './config/mongoose';
 import ticketsRouter from './routes/tickets';
+import path from 'path';
 
 connectMongo();               
 
 const app = express();
 app.use(express.json());
 app.use('/tickets', ticketsRouter);
+app.use(express.static(path.join(__dirname, '../public')));
 
 const server = app.listen(3000, () =>
   console.log('API on http://localhost:3000'),
